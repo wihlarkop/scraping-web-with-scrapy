374b79cab0a978b7776de3c105d53d04770ed49d
wihlarkop <wihlarko26@gmail.com>
2020-06-30 23:00:00 +0700

    update quotes scraping

--- quotes/quotes/spiders/quotes_spider.py
+++ quotes/quotes/spiders/quotes_spider.py
@@ -4,8 +4,16 @@ import scrapy
 
 class QuotesSpiderSpider(scrapy.Spider):
     name = 'quotes_spider'
-    allowed_domains = ['http://quotes.toscrape.com']
-    start_urls = ['http://http://quotes.toscrape.com']
+    allowed_domains = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/']
 
     def parse(self, response):
-        pass
+        all_div_quotes = response.css('div.quote')
+        quotes = all_div_quotes.css('span.text::text').extract()
+        author = all_div_quotes.css('.author::text').extract()
+        tags = all_div_quotes.css('.tag::text').extract()
+        yield {
+            'quotes': quotes,
+            'author': author,
+            'tags': tags,
+        }
